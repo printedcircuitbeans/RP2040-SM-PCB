d77ef3730ff72e78853c26edfae56d76180364bd
printedcircuitbeans <140086246+printedcircuitbeans@users.noreply.github.com>
2025-02-23 00:16:47 +0100

    Update WATCHBasic01.ino

--- WATCHBasic01.ino
+++ WATCHBasic01.ino
@@ -5,12 +5,12 @@
 #include <TinyGPSPlus.h>
 #include "MAX30100_PulseOximeter.h"
 #include <Wire.h>
-
+#include <Fonts/FreeMonoBold12pt7b.h>
 
 //Screen setup
 #define TFT_CS 3
 #define TFT_RST 2
-#define TFT_DC 20
+#define TFT_DC 12
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 TinyGPSPlus gps;
@@ -19,124 +19,95 @@ TinyGPSPlus gps;
 MAX30100 sensor;
 PulseOximeter pox;
 
-int Bt1 = 6;
-int Bt2 = 7;
-int Bt3 = 20;
-int Bt4 = 21;
+const int Bt1 = 6;
+const int Bt2 = 7;
+const int Bt3 = 20;
+const int Bt4 = 21;
+
+int hour = 0;
+int minut = 0;
+int second = 0;
 unsigned long mtime;
 int ScrMOS = 10;
 bool scrn0N = 1;
 int i = 0;
-int dgWheel = 0;
+
+int STAT2 = 23;
+int STAT1 = 24;
+
+
+void gpsSleep() {
+    uint8_t standbyCmd[] = { 0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4B, 0xDC };
+    Serial1.write(standbyCmd, sizeof(standbyCmd));
+}
 
 void setup() {
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST7735_BLACK);
 
   GPS_SERIAL.begin(9600);
-
-  pox.begin();
-
-
   pinMode(Bt1, INPUT);
   pinMode(Bt2, INPUT);
+  pinMode(Bt3, INPUT);
   pinMode(Bt4, INPUT);
+  pinMode(STAT1, INPUT);
+  pinMode(STAT2, INPUT);
   pinMode(ScrMOS, OUTPUT);
   digitalWrite(ScrMOS, scrn0N);
-
-  sensor.setMode(MAX30100_MODE_SPO2_HR);
-  sensor.setLedsCurrent(MAX30100_LED_CURR_50MA, MAX30100_LED_CURR_50MA);
 }
-void drawBioUI(int16_t x, int16_t y, int16_t hb, int16_t oxy) {
-  tft.fillCircle(x + 30, y + 30, 20, ST7735_RED);
-  tft.fillCircle(x + 30, y + 30, 10, ST7735_BLACK);
-  tft.fillCircle(x + 80, y + 30, 20, ST7735_CYAN);
-  tft.fillCircle(x + 80, y + 30, 10, ST7735_BLACK);
 
-  tft.setTextColor(ST7735_WHITE, 0);
-  tft.setCursor(x + 26, y + 27);
-  tft.print(hb);
-  tft.setCursor(x + 76, y + 27);
-  tft.print(oxy);
-}
 void loop() {
-
-
   mtime = millis();
   while (digitalRead(Bt1)) {
     i = millis() - mtime;
     if (scrn0N && digitalRead(Bt1) && i >= 1000) {
+      gpsSleep(); 
       scrn0N = 0;
       delay(50);
       tft.fillScreen(ST7735_BLACK);
     } else if (digitalRead(Bt1) && i >= 100) {
       scrn0N = 1;
+      Serial1.write(0xFF);  // Wakey wakey eggs and bakey gps!
       delay(50);
-    }
+          }
   }
-
+  
   digitalWrite(ScrMOS, scrn0N);
   if (scrn0N) {
-    tft.setCursor(0, 0);
+    tft.setFont(&FreeMonoBold12pt7b);
     tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    tft.fillRect(0, 0, 160, 50, ST7735_BLACK);
+    tft.setCursor(10, 30);
+    tft.print(hour);
+    tft.print(":");
+    tft.print(minut);
+    tft.print(":");
+    tft.println(second);
+    tft.setFont();
 
-
-
-
-
-
+    if(digitalRead(STAT1) && !digitalRead(STAT2)){ 
+       tft.setTextColor(ST7735_GREEN,ST7735_BLACK);
+      tft.println("Charge Complete    ");
+        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    }
+    if(digitalRead(STAT2) && !digitalRead(STAT1)){
+        tft.setTextColor(ST7735_RED, ST7735_BLACK);
+      tft.println("Charging           ");
+        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    }
+    if(digitalRead(STAT1) && digitalRead(STAT1)){
+              tft.setTextColor(ST7735_BLUE, ST7735_BLACK);
+      tft.println("Charger disconnected");
+        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    }
     while (GPS_SERIAL.available()) {
       char c = GPS_SERIAL.read();
       gps.encode(c);  // Feed data to TinyGPS++
     }
-
-    tft.println("----------------");
-    tft.print("Time :");
-    tft.print(gps.time.hour());
-    tft.print(" : ");
-    tft.print(gps.time.minute());
-    tft.print(" : ");
-    tft.println(gps.time.second());
-
-    /*
-    tft.print("Latitude: ");
-    tft.println(gps.location.lat(), 12);
-    tft.print("Longitude: ");
-    tft.println(gps.location.lng(), 12);
-    tft.print("sattlock: ");
-    tft.println(gps.satellites.value(), 2);
-*/
-
-    tft.setCursor(0, 50);
-    if (digitalRead(Bt2) && dgWheel != 0) {
-      dgWheel--;
-      delay(50);
-    }
-    if (digitalRead(Bt4) && dgWheel != 3) {
-      dgWheel++;
-      delay(50);
-    }
-    tft.println(" BIOMETRIC SENSOR   ");
-    tft.println(" BME 680 GAS        ");
-    tft.println(" SD CARD FS BASIC   ");
-    tft.println(" GPS DATA DSP       ");
-    tft.println(dgWheel);
-    tft.drawRect(0, (dgWheel * 8) + 49, 120, 9, ST7735_WHITE);
-    mtime = millis();
-  }
-  if (digitalRead(Bt1)) {
-    if (dgWheel == 0) {
-      delay(100);
-      tft.fillScreen(ST7735_BLACK);
-      sensor.resume();
-      while (digitalRead(Bt1) != 1) {
-
-        pox.update();
-        delay(20);
-        drawBioUI(10, 50, pox.getHeartRate(), pox.getSpO2());
-      }
-      sensor.shutdown();
-      tft.fillScreen(ST7735_BLACK);
+    if (gps.date.isValid()) {
+      hour = gps.time.hour() + 1;
+      minut = gps.time.minute();
+      second = gps.time.second();
     }
   }
 }
