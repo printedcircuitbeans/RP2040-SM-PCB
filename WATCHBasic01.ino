018c88f9aa687a81245aaf92b271f499884376db
printedcircuitbeans <140086246+printedcircuitbeans@users.noreply.github.com>
2025-02-24 01:31:35 +0100

    Update WATCHBasic01.ino
    Added a very inefficient GPS mapping demo
    - Not at all optimized
    - Untested : was unable to get a GPS lock with the watch at 1:30 AM in my garden.
    
    Should dynamically resize as your area of "exploration" gets bigger.
    Maps for 60 seconds at 500 ms intervalls before running out of space. (will implement sd card saving at a later date)

--- WATCHBasic01.ino
+++ WATCHBasic01.ino
@@ -24,6 +24,15 @@ const int Bt2 = 7;
 const int Bt3 = 20;
 const int Bt4 = 21;
 
+float gpsramlat[120];
+float gpsramlng[120];
+float gpsminy = 0;
+float gpsminx = 0;
+float gpsmaxx = 0;
+float gpsmaxy = 0;
+int gpsconstx = 0;
+int gpsconsty = 0;
+
 int hour = 0;
 int minut = 0;
 int second = 0;
@@ -31,15 +40,18 @@ unsigned long mtime;
 int ScrMOS = 10;
 bool scrn0N = 1;
 int i = 0;
+int ii = 0;
 
+int dgWheel = 0;
 int STAT2 = 23;
 int STAT1 = 24;
 
 
 void gpsSleep() {
-    uint8_t standbyCmd[] = { 0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4B, 0xDC };
-    Serial1.write(standbyCmd, sizeof(standbyCmd));
+  uint8_t standbyCmd[] = { 0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4B, 0xDC };
+  Serial1.write(standbyCmd, sizeof(standbyCmd));
 }
+
 
 void setup() {
   tft.initR(INITR_BLACKTAB);
@@ -61,7 +73,7 @@ void loop() {
   while (digitalRead(Bt1)) {
     i = millis() - mtime;
     if (scrn0N && digitalRead(Bt1) && i >= 1000) {
-      gpsSleep(); 
+      gpsSleep();
       scrn0N = 0;
       delay(50);
       tft.fillScreen(ST7735_BLACK);
@@ -69,9 +81,9 @@ void loop() {
       scrn0N = 1;
       Serial1.write(0xFF);  // Wakey wakey eggs and bakey gps!
       delay(50);
-          }
+    }
   }
-  
+
   digitalWrite(ScrMOS, scrn0N);
   if (scrn0N) {
     tft.setFont(&FreeMonoBold12pt7b);
@@ -85,20 +97,20 @@ void loop() {
     tft.println(second);
     tft.setFont();
 
-    if(digitalRead(STAT1) && !digitalRead(STAT2)){ 
-       tft.setTextColor(ST7735_GREEN,ST7735_BLACK);
-      tft.println("Charge Complete    ");
-        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    if (digitalRead(STAT1) && !digitalRead(STAT2)) {
+      tft.setTextColor(ST7735_GREEN, ST7735_BLACK);
+      tft.println("Charge Complete     ");
+      tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
     }
-    if(digitalRead(STAT2) && !digitalRead(STAT1)){
-        tft.setTextColor(ST7735_RED, ST7735_BLACK);
-      tft.println("Charging           ");
-        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    if (digitalRead(STAT2) && !digitalRead(STAT1)) {
+      tft.setTextColor(ST7735_RED, ST7735_BLACK);
+      tft.println("Charging            ");
+      tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
     }
-    if(digitalRead(STAT1) && digitalRead(STAT1)){
-              tft.setTextColor(ST7735_BLUE, ST7735_BLACK);
-      tft.println("Charger disconnected");
-        tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
+    if (digitalRead(STAT1) && digitalRead(STAT2)) {
+      tft.setTextColor(ST7735_BLUE, ST7735_BLACK);
+      tft.println("Charger disconnected ");
+      tft.setTextColor(ST7735_WHITE, ST7735_BLACK);
     }
     while (GPS_SERIAL.available()) {
       char c = GPS_SERIAL.read();
@@ -108,6 +120,61 @@ void loop() {
       hour = gps.time.hour() + 1;
       minut = gps.time.minute();
       second = gps.time.second();
+    }
+    if (digitalRead(Bt2) && dgWheel != 0) {
+      dgWheel--;
+      delay(50);
+    }
+    if (digitalRead(Bt4) && dgWheel != 3) {
+      dgWheel++;
+      delay(50);
+    }
+
+
+    if (digitalRead(Bt1) && dgWheel == 0) {  //the ultimate UI skullfucking
+      delay(100);
+      i = 0;
+
+      while (!digitalRead(Bt1)) {
+        delay(500);
+        tft.fillScreen(ST7735_BLACK);
+        tft.setCursor(0, 0);
+        tft.println("GPS UTILITY BASIC");
+        tft.println(gps.location.lat());
+        tft.println(gps.location.lng());
+        tft.print(gpsminx);
+        tft.print(",");
+        tft.print(gpsminy);
+        tft.print(",");
+        tft.print(gpsmaxx);
+        tft.print(",");
+        tft.println(gpsmaxy);
+        gpsramlat[i] = gps.location.lat();
+        gpsramlng[i] = gps.location.lng();
+
+        for (ii = 0; ii != 120; ii++) {
+          if (gpsramlat[i] < gpsminx) {  //this shit will only resize in one direction
+            gpsminx = gpsramlat[i];
+          }
+          if (gpsramlat[i] > gpsmaxx) {
+            gpsmaxx = gpsramlat[i];
+          }
+          if (gpsramlng[i] < gpsminy) {
+            gpsminy = gpsramlng[i];
+          }
+          if (gpsramlng[i] > gpsmaxy) {
+            gpsmaxy = gpsramlng[i];
+          }
+        }
+        ii = 0;
+        while (i != ii) {
+          tft.drawLine((gpsramlat[ii] - gpsminx) * (128 / (gpsmaxx - gpsminx)), (gpsramlng[ii] - gpsminy) * (128 / (gpsmaxy - gpsminy)), (gpsramlat[ii + 1] - gpsminx) * (128 / (gpsmaxx - gpsminx)), (gpsramlng[ii + 1] - gpsminy) * (128 / (gpsmaxy - gpsminy)), ST7735_ORANGE);
+          ii++;
+        }
+
+        tft.drawRect(0, 32, 128, 128, ST7735_WHITE);
+      }
+      tft.fillScreen(ST7735_BLACK);
     }
   }
 }
